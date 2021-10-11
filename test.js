edb951fa88d8de3356980d6d771ec458379c91f9
Alex-Za <argiluckone@gmail.com>
2021-10-11 16:55:39 +0300

    Initial commit

---
+++ test.js
@@ -0,0 +1,30 @@
+"use strict"
+
+describe("pow", () => {
+
+    it("для отрицательных n возвращает NaN", () => {
+        assert.isNaN(pow(2, -1));
+    });
+
+    it("для дробных n возвращает NaN", () => {
+        assert.isNaN(pow(2, 1.5));
+    });
+
+    describe("возводит x в степень 3", () => {
+
+        function makeTest(x) {
+            let expected = x * x * x;
+            it(`${x} в степени 3 будет ${expected}`, () => {
+                assert.equal(pow(x, 3), expected);
+            });
+        }
+
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+        }
+
+    });
+
+
+
+});
